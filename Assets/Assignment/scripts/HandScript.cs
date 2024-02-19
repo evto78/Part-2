8c9ba3068a028e202d2fb0538566d08cba65f0e4
evto78 <devesto78@gmail.com>
2024-02-19 15:06:42 -0500

    all animations are now properly playing for the hand.

--- Assets/Assignment/scripts/HandScript.cs
+++ Assets/Assignment/scripts/HandScript.cs
@@ -6,14 +6,53 @@ using UnityEngine;
 
 public class HandScript : MonoBehaviour
 {
+    Animator animator;
+    float damagedTimer = 0f;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetBool("Selecting", false);
     }
 
     void Update()
     {
+        if (damagedTimer > 0f)
+        {
+            damagedTimer -= Time.deltaTime;
+        }
+        else
+        {
+            animator.SetBool("Damaged", false);
+        }
         transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.Translate(0, 0, 10);
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("Holding", true);
+        }
+        else
+        {
+            animator.SetBool("Holding", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision detected");
+        if (collision.tag == "interactable")
+        {
+            animator.SetBool("Selecting", true);
+        }
+        else if (collision.tag == "dangerous")
+        {
+            animator.SetBool("Damaged", true);
+            damagedTimer = 3f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("Selecting", false);
     }
 }
