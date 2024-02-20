7e38634fc38ccee6e2da28f0692bbf4cb75cfa0a
evto78 <devesto78@gmail.com>
2024-02-19 23:20:02 -0500

    Tile scene made and decorated, text and button are not functional yet.

---
+++ Assets/Assignment/scripts/Fluctuate.cs
@@ -0,0 +1,35 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fluctuate : MonoBehaviour
+{
+    public float i = -0.5f;
+    bool fluctuateDirUp = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fluctuateDirUp)
+        {
+            i += 0.002f;
+        }
+        else
+        {
+            i -= 0.002f;
+        }
+
+        if (i > 0.3f)
+        {
+            fluctuateDirUp = false;
+        }
+        else if (i < -0.3f)
+        {
+            fluctuateDirUp = true;
+        }
+
+        transform.Translate(0, i, 0);
+    }
+}
--- Assets/Assignment/scripts/HandScript.cs
+++ Assets/Assignment/scripts/HandScript.cs
@@ -51,6 +51,15 @@ public class HandScript : MonoBehaviour
         }
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "dangerous")
+        {
+            animator.SetBool("Damaged", true);
+            damagedTimer = 3f;
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         animator.SetBool("Selecting", false);
