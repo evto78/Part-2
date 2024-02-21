2d9cd51a6dec4cf40753d933c16a6c4a74fb073c
vandyeva <vandyeva@OA-S141-22>
2024-02-21 15:56:30 -0500

    goalkeeper moves but goes out of bounds

---
+++ Assets/Week 7/scripts/GoalkeeperController.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalkeeperController : MonoBehaviour
+{
+    public Rigidbody2D goalkeeperRb;
+    Vector2 direction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = ((Vector2)transform.position - (Vector2)SportController.CurrentSelection.transform.position) / 2;
+        goalkeeperRb.position = (Vector2)transform.position + direction;
+    }
+}
